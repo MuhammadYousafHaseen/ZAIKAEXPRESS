2985a3b2f1a9dea5a7a7bd0cdc62b62d940f40e3
MuhammadYousafHaseen <muhammadyousaf71eb@gmail.com>
2025-04-10 06:02:41 +0500

    Update signUpSchema.ts

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,7 +4,7 @@ export const nameValidation = z
 .string()
 .min(3, {message: 'Username must be at least 3 characters long'})
 .max(30, {message: 'Username must be at most 30 characters long'})
-.regex(/^[a-zA-Z0-9_ ]+$/, {message: 'Name must contain only letters, numbers, and underscores and spaces'});
+.regex(/^[a-zA-Z0-9_]+$/, {message: 'Name must contain only letters, numbers, and underscores and not spaces'});
 
 
 export const signUpSchema = z.object({
